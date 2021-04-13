33e19974c534dd638176a916d2f75d34e76e4638
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-04-13 03:51:33 +0200

    Add missing closing tag

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
-  <p style="float: left;"><ion-icon name="git-branch"></ion-icon><?php echo exec('git log -1 --pretty=\'%cd-%h\' --date=format:\'%y.%m\''); ?>
+  <p style="float: left;"><ion-icon name="git-branch"></ion-icon><?php echo exec('git log -1 --pretty=\'%cd-%h\' --date=format:\'%y.%m\''); ?></p>
   <p style="float: right;">Allzeit Voran!</p>
   <div style="clear: both;"></div>
 </footer>
