59f7d53f40dca4bf37c202f5609589872478d3b8
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-07-09 02:44:06 +0200

    Use SHA1 for sensitive information

--- util.php
+++ util.php
@@ -83,8 +83,8 @@ function append_vote(string $pin, mixed $vote_contents)
  // create vote entry
  $vote_entry = [
   "time"       => time(),
-  "ip"         => md5($_SERVER['REMOTE_ADDR']),
-  "session-id" => md5(session_id()),
+  "ip"         => sha1($_SERVER['REMOTE_ADDR']),
+  "session-id" => sha1(session_id()),
   "contents"   => $parsed,
  ];
 
