08ac5572ef483276328324424149138a2c7feaa7
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2021-07-14 18:09:27 +0200

    Clean up unused files

--- footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="uk-section uk-section-muted uk-margin-xlarge-top">
-<div class="uk-container">
-  <p><span uk-icon="git-branch"></span><?php echo exec('git log -1 --pretty=\'%cd-%h\' --date=format:\'%y.%m\''); ?></p>
-  <!--<p cass="uk-position-right">Allzeit Voran!</p>-->
-</div>
-</footer>
